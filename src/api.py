68d0c7494562832370371dc5094bcd4f746c51b4
SilverAnton <cu4hawk@gmail.com>
2024-03-31 11:04:22 +0700

    add class API

---
+++ src/api.py
@@ -0,0 +1,22 @@
+from base_class import BaseVacancy
+import requests
+import json
+
+
+class API(BaseVacancy):
+    def __init__(self):
+        self.__url = 'https://api.hh.ru/vacancies'
+        self.__headers = {'User-Agent': 'HH-User-Agent'}
+        self.params = {'text': '', 'page': 0, 'per_page': 100}
+        super().__init__(self)
+        self.vacancies = []
+
+    def add_vacancy(self, keyword):
+        self.params['text'] = keyword
+        response = requests.get(self.__url, headers=self.__headers, params=self.params)
+        vacancies = response.json()['items']
+        self.vacancies.append(vacancies)
+
+    def save_as_json(self):
+        with open('vacancies.json', 'w', encoding="UTF8") as file:
+            json.dump(self.vacancies[0], file, ensure_ascii=False, indent=4)
